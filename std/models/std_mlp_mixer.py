91d0eb05e7a1a6996800af342d54fde5623d1d8e
devrimcavusoglu <devrimcavusoglu@gmail.com>
2023-05-26 01:03:30 +0300

    WIP, trying to figure out what paper writes.

--- std/models/std_mlp_mixer.py
+++ std/models/std_mlp_mixer.py
@@ -8,30 +8,22 @@ pair = lambda x: x if isinstance(x, tuple) else (x, x)
 
 
 class PreNormResidual(nn.Module):
-    def __init__(self, dim, fn, distillation: str = None):
+    def __init__(self, dim, fn):
         super().__init__()
         self.fn = fn
         self.norm = nn.LayerNorm(dim)
-        self.distillation = distillation
 
-    def forward(self, x, t):
-        if self.distillation == "spatial":
-            zt = torch.cat([x, t], 1)
-            zt = self.fn(self.norm(zt)) + t
-            return zt[:, :-1, :], zt[:, -2:-1, :]  # (z, ts)
-
-        zt = torch.cat([x, t], -1)
-        zt = self.fn(self.norm(zt)) + t
-        return zt[:, :, :-1], zt[:, :, -2:-1]  # (z, ts)
+    def forward(self, x):
+        return self.fn(self.norm(x)) + x
 
 
 class FFN(nn.Module):
-    def __init__(self, dim, inner_dim_factor: float = 4, dropout: float = 0.0, mixing_type: str = "channel"):
+    def __init__(self, dim, inner_dim_factor: float = 4, dropout: float = 0.0, mixing_type: str = "channel", distillation: bool = False):
         super().__init__()
         inner_dim = int(dim * inner_dim_factor)
         layer_dense = nn.Linear if mixing_type == "channel" else partial(nn.Conv1d, kernel_size=1)
         self.dense1 = layer_dense(dim, inner_dim)
-        self.dense2 = layer_dense(inner_dim, dim)
+        self.dense2 = layer_dense(inner_dim, dim) if not distillation else layer_dense(inner_dim, 1)
         self.gelu = nn.GELU()
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
@@ -43,16 +35,30 @@ class FFN(nn.Module):
         x = self.dropout2(x)
         return x
 
-class MixerBlock(nn.Module):
-    def __init__(self, dim, n_patches, f_spatial_expansion: float = 4, f_channel_expansion: float = 4, dropout: float = 0.0):
-        super().__init__()
-        self.token_mixer = PreNormResidual(dim, FFN(n_patches+1, f_spatial_expansion, dropout, mixing_type="spatial"), distillation="spatial")
-        self.channel_mixer = PreNormResidual(dim+1, FFN(dim+1, f_channel_expansion, dropout, mixing_type="channel"), distillation="channel")
 
-    def forward(self, x, ts, tc):
-        x, ts = self.token_mixer(x, ts)
-        x, tc = self.channel_mixer(x, tc)
-        return x, ts, tc
+class MixerBlock(nn.Module):
+    def __init__(self, dim, n_patches, ds: float = 4, dc: float = 0.5, dropout: float = 0.0):
+        super().__init__()
+        self.token_mixer = PreNormResidual(dim, FFN(n_patches, ds, dropout, mixing_type="spatial"))
+        self.channel_mixer = PreNormResidual(dim, FFN(dim, dc, dropout, mixing_type="channel"))
+
+        self.spatial_dist = FFN(n_patches + 1, ds, dropout, mixing_type="spatial", distillation=True)
+        self.spatial_dist_norm = nn.LayerNorm(dim)
+
+        self.channel_dist = FFN(dim + 1, dc, dropout, mixing_type="channel", distillation=True)
+        self.channel_dist_norm = nn.LayerNorm(dim + 1)
+
+    def forward(self, z, ts, tc):
+        u = self.token_mixer(z)
+        z = self.channel_mixer(u)
+
+        zts = torch.cat((z, ts), 1)
+        ts = self.spatial_dist(self.spatial_dist_norm(zts)) + ts
+
+        ztc = torch.cat((z, tc), -1)
+        tc = self.channel_dist(self.channel_dist_norm(ztc)) + tc
+
+        return z, ts, tc
 
 
 class STDMLPMixer(nn.Module):
@@ -96,9 +102,9 @@ if __name__ == "__main__":
 
     device = torch.device("cuda")
     image_size = 224
-    mixer = STDMLPMixer(image_size=image_size, channels=3, patch_size=16, dim=512, depth=2).to(device)
+    mixer = STDMLPMixer(image_size=image_size, channels=3, patch_size=16, dim=512, depth=2, num_classes=10).to(device)
     input_size = (1, 3, image_size, image_size)  # b,c,h,w
-    # summary(mixer, input_size=input_size, device=device)
+    summary(mixer, input_size=input_size, device=device)
     torch.manual_seed(3)
     x = torch.randn(*input_size, device=device)
     y = mixer(x)
